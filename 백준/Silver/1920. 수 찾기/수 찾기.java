f02c1da667eb983bf4c3e56a079bf6ad6bce39c3
MoRinGnA <59137820+MoRinGnA@users.noreply.github.com>
2026-03-09 16:26:53 +0900

    [Silver IV] Title: 수 찾기, Time: 1696 ms, Memory: 193980 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedInputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+
+        int N = scanner.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i=0;i<N;i++) {
+            map.put(scanner.nextInt(), 0);
+        }
+
+        int M = scanner.nextInt();
+
+        for (int i=0;i<M;i++) {
+
+            int C = scanner.nextInt();
+
+            if (map.containsKey(C)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+        scanner.close();
+    }
+}
