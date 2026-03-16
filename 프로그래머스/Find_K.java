1d9d7f2414cc0eb44d06e06f0d5921f4566855e4
Lee, ChanHyeong <iambiginer@gmail.com>
2026-03-16 16:49:12 +0900

    첫번째 버전

---
+++ 프로그래머스/Find_K.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int index = 0; index < commands.length; index++) {
+            int i = commands[index][0];
+            int j = commands[index][1];
+            int k = commands[index][2];
+
+            int[] NewArray = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(NewArray);
+            answer[index] = NewArray[k - 1];
+        }
+        return answer;
+    }
+}
