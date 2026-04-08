c16f2bc020cdbd8a66d2a8b8b1b17cf15df9c804
Lee, ChanHyeong <iambiginer@gmail.com>
2026-04-08 17:27:48 +0900

    첫번쨰 풀이

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n, s;
+    public static int[] array;
+    public static int count = 0;
+
+    public static void seq(int index, int sum) {
+        if (index == n) {
+            if (sum == s) {
+                count++;
+            }
+            return;
+        }
+        seq(index + 1, sum);
+        seq(index + 1, sum + array[index]);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        s = sc.nextInt();
+
+        array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        seq(0, 0);
+
+        System.out.println(count);
+    }
+}
