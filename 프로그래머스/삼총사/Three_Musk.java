7b002887890de6aa5128721414d9530cb4227e36
Lee, ChanHyeong <iambiginer@gmail.com>
2026-04-15 15:43:16 +0900

    재귀로 구현

--- 프로그래머스/삼총사/Three_Musk.java
+++ 프로그래머스/삼총사/Three_Musk.java
@@ -1,17 +1,40 @@
-class Solution {
-    public int solution(int[] number) {
-        int answer = 0;
-        int N = number.length;
+// class Solution {
+//     public int solution(int[] number) {
+//         int answer = 0;
+//         int N = number.length;
 
-        for (int i = 0; i < N - 2; i++) {
-            for (int j = i + 1; j < N - 1; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
-            }
-        }
+//         for (int i = 0; i < N - 2; i++) {
+//             for (int j = i + 1; j < N - 1; j++) {
+//                 for (int k = j + 1; k < N; k++) {
+//                     if (number[i] + number[j] + number[k] == 0) {
+//                         answer++;
+//                     }
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+class Solution {
+    int answer = 0;
+
+    public int solution(int[] number) {
+        Three(number, 0, 0);
         return answer;
+
+    }
+
+    public void Three(int[] number, int s, int c) {
+        if (c == 3) {
+            if (s == 0) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = 0; i < number.length; i++) {
+            Three(number, s + number[i], c + 1);
+        }
     }
 }
