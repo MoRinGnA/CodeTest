868e6c40895b85ee7e73281204773ee2876d6d78
Lee, ChanHyeong <iambiginer@gmail.com>
2026-04-22 15:57:53 +0900

    첫번째 풀이

---
+++ 프로그래머스/신규 아이디 추천/New_id.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+
+        if (answer.length() == 0) {
+            answer = "a";
+        }
+
+        answer = answer.substring(0, 15);
+        answer = answer.replaceAll("[.]$", "");
+
+        for (int i = answer.length(); i < 3; i++) {
+            answer = answer.charAt(answer.length() - 1);
+        }
+        return answer;
+    }
+}
