039c31a0ff6d21223841cb30567add1d174054ce
Lee, ChanHyeong <iambiginer@gmail.com>
2026-04-29 17:33:07 +0900

    두번째 풀이

--- 프로그래머스/입국심사/Immi.java
+++ 프로그래머스/입국심사/Immi.java
@@ -2,26 +2,33 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        int left = 1, right = 0, middle = 0;
+        long left = 1, right = 0, middle = 0;
         long answer = 0;
         Arrays.sort(times);
-        
+
         right = times[times.length - 1] * n;
-        middle = (left + right) / 2;
-        
-        answer = compare(n, times, middle);    
-        return answer;
-    }
-    
-    public long compare(int n, int[] times, long middle) {
-        long count = 0;
-        for (int i=0;i<times.length;i++) {
-            count += middle/times[i];
-            
-            while(count > n){
-                break;
+        while (true) {
+            if (compare(n, times, middle) > 0) {
+                if (compare(n, times, middle - 1) < 0) {
+                    break;
+                }
+                middle--;
+            } else {
+                middle++;
             }
         }
-        return count;
+        return middle;
+    }
+
+    public long compare(int n, int[] times, long middle) {
+        long count = 0;
+        for (int i = 0; i < times.length; i++) {
+            count += middle / times[i];
+
+            if (count >= n) {
+                return 1;
+            }
+        }
+        return -1;
     }
 }
