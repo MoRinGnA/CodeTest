972a28f93e7d12b28d7663dee7257ec9111a1b8a
Lee, ChanHyeong <iambiginer@gmail.com>
2026-05-06 15:26:58 +0900

    첫번째 풀이 (#14)

---
+++ 프로그래머스/게임 맵 최단거리/maps.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    static int ROW, COL;
+    static int[][] A;
+
+    static int BFS() {
+        var visited = new boolean[ROW][COL];
+        var queue = new ArrayDeque<int[]>();
+        queue.add(new int[] { 0, 0, 0 });
+        while (queue.size() > 0) {
+            int[] u = queue.remove();
+            int r = u[0], c = u[1], d = u[2];
+            if (A[r][c] == 0)
+                continue;
+            if (visited[r][c])
+                continue;
+            visited[r][c] = true;
+            if (r == ROW - 1 & c == COL - 1)
+                return d;
+            if (r > 0)
+                queue.add(new int[] { r - 1, c, d + 1 });
+            if (c > 0)
+                queue.add(new int[] { r, c - 1, d + 1 });
+            if (r < ROW - 1)
+                queue.add(new int[] { r + 1, c, d + 1 });
+            if (c < COL - 1)
+                queue.add(new int[] { r, c + 1, d + 1 });
+        }
+        return -1;
+    }
+
+    public int solution(int[][] maps) {
+        ROW = maps.length;
+        COL = maps[0].length;
+        A = maps;
+        return BFS();
+    }
+}
